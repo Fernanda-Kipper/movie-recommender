65ad37950ff6a2955d8a4954d7b1c00a530410be
Fernanda <nanda.kipper@gmail.com>
2024-07-09 16:39:21 -0300

    feat: initial commit

---
+++ src/main/java/com/fernandakipper/movie_recommender/MovieRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.movie_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandakipper/movie_recommender/MovieRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.movie_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
